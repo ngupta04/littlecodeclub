fcf3d7db430e0845f25f88fec5c16fe2e3b7f70d
PG <pg@PGs-MacBook-Air.local>
2020-02-22 14:53:04 -0700

    Updates

---
+++ precache-manifest.4fa58b50c72de771838c2654fb35f474.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0998e842a65f705e9d0577cae0532298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49599df48897a3491db3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bfe692cc1972676dfdb7",
+    "url": "/static/js/2.1e485786.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.1e485786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49599df48897a3491db3",
+    "url": "/static/js/main.e3f709d0.chunk.js"
+  },
+  {
+    "revision": "e941a900913f0742ff50",
+    "url": "/static/js/runtime-main.6d1bca60.js"
+  },
+  {
+    "revision": "4322c536bce686130af5de6e84f602c5",
+    "url": "/static/media/1858.4322c536.jpg"
+  },
+  {
+    "revision": "b1555ae8fb9b591bf778bd3bc650ab7a",
+    "url": "/static/media/1859.b1555ae8.jpg"
+  },
+  {
+    "revision": "048701d647ce66adbe0324007e9837ae",
+    "url": "/static/media/1860.048701d6.jpg"
+  }
+]);
